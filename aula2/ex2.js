95e68cd72e7a02732f217bcf9b7baa2cde401496
moraguinho001 <motta.mariane@escola.pr.gov.br>
2026-06-11 20:45:46 -0300

    backend_mariane

---
+++ aula2/ex2.js
@@ -0,0 +1,7 @@
+const input = require("readline-sync")
+const senha = input.question(`senha: `)
+if(senha.length >= 8){
+    console.log(`senha valida`)
+} else {
+    console.log(`senha invalida - minimo de 8 caracteres`)
+}
